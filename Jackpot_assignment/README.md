217256cf9caccd27cc02b0616062e98f4f303dab
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2021-03-29 00:42:48 +0300

    final checkup

---
+++ Jackpot_assignment/README.md
@@ -0,0 +1,3 @@
+# JACKPOT
+Randomly specified number of numbers will get selected by computer. if eachtime One get selected then person will win.
+task: find average round need to play to win the jackpot. calculate probality of winning / losing the game
--- Jackpot_assignment/scripts.js
+++ Jackpot_assignment/scripts.js
@@ -1,8 +1,5 @@
 
-
-//1. Create an **array of 5 elements**, each element is a random number of either 1 or 0
-
-let randomizedRange = 2
+let randomizedRange = 6
 let randomNum = () => Math.floor( Math.random()*randomizedRange)
 let randomArray = []
 let counter = 0;
@@ -11,48 +8,15 @@ while( counter <5){
     randomArray[counter] = randomNum()
     counter++
 }
-console.log('Ans of Q1: randomArray', randomArray)
 
 // 2. Create a while loop, that goes through each element in the array, check if they are 1 or 0
 //3. Also create a counter variable to keep track of the number of 1 and 0 values in the array
 
+
 let i = 0
-let countOne = 0
-let countZero = 0
-
-while(i<randomArray.length ){
-   if(randomArray[i]=== 0){
-        console.log('this array element is 1')
-        countZero++
-    }
-    else {
-        console.log('this array element is 0')
-        countOne++
-    }
-    i++
-}
-console.log(`Ans of Q2 & 3: ${countZero} Zero and ${countOne} One`)
-
-
-// 4. If the number of 1s is 5, announce the jackpot prize
-
-if(countOne===5) console.log('Ans of Q4: congrats you have won the Jackpot')
-
-// 5. If the number of 0s is 5, announce the congrat message
-
-if(countZero===5) console.log(' Ans of Q5: congrats you have all zero number! Goodluck with Jackpot nextime!!')
-
-//6. If the number of 1s is 3 < x < 5, announce smaller prize
-
-if(countOne>3 && countOne < 5) console.log('Ans of Q6: congrats you have won a smaller prize!')
-
-//7. Create a while loop that will run the above application for an undefinite number of time, and only break out of the loop if the jackpot prize is won. 
-//8. Create a variable to keep track of many rounds have been played in order to win the jackpot
-
 const RoundToWinJackpot = () => {
     let count= 0
     let round = 0
-    randomizedRange = 6
     let randomArr = [ randomNum(), randomNum(), randomNum(), randomNum(), randomNum()]
     
      let j = 0
@@ -94,4 +58,4 @@ console.log(`Ans of Q10: average round need to win Jackpot is ${avgRoundToWin}. 
 let jackpotPrize = 30000
 let result = 30000>avgMoneyToWin? 'profit' : 'loss'
 let amount = result=='loss'? avgMoneyToWin-30000 : 30000 - avgMoneyToWin
-  console.log(`Ans of Q11: Your ${result} from jackpotPrize is ${amount}`)
+ console.log(`Ans of Q11: Your ${result} from jackpotPrize is ${amount}`)
